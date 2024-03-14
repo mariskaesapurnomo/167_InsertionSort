1dbcc00e4e2e6ac1e108c740a754c55c8c5a03ed
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-14 12:33:28 +0700

    membuat prosedur memanggil

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -65,9 +65,17 @@ void input() {                                          // Procedure Input
         cout << "Element Array yang telah tersusun" << endl;        // Output ke layar
         cout << "=================================" << endl;        // Output ke layar
 
-        for (int j = 0; j, n; j++) {            // Looping dengan j dimulai dari 0 hingga n-1
+        for (int j = 0; j < n; j++) {            // Looping dengan j dimulai dari 0 hingga n-1
             cout << arr[j] << endl;             // Output ke layar
         }
         cout << endl;                           // Output baris kosong
 
-}
+    }
+
+    int main()
+    {
+
+        input();                // Memanggil input
+        insertionsort();        // Memanggil insertionsort
+        display();              // Memanggil display
+    }
