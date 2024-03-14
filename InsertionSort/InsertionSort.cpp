5d802d6ef82b874b13e81d62a847f46f55530966
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-14 12:26:17 +0700

    membuat prosedur display

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -58,3 +58,16 @@ void input() {                                          // Procedure Input
             }
         }
     }
+
+    void display() {                            // Procedure Display
+        cout << endl;                           // Output baris kosong
+        cout << "\n===============================" << endl;       //Output ke layar
+        cout << "Element Array yang telah tersusun" << endl;        // Output ke layar
+        cout << "=================================" << endl;        // Output ke layar
+
+        for (int j = 0; j, n; j++) {            // Looping dengan j dimulai dari 0 hingga n-1
+            cout << arr[j] << endl;             // Output ke layar
+        }
+        cout << endl;                           // Output baris kosong
+
+}
