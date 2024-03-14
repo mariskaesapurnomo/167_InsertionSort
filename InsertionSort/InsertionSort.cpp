7d9f2f047687d0528f63b8150527f9598c261215
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-14 10:13:05 +0700

    Add project files.

---
+++ InsertionSort/InsertionSort.cpp
@@ -0,0 +1,9 @@
+// InsertionSort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int arr[20];                                            // Membuat Array dengan panjang data 20
+int n;                                                  // Membuat variabel inputan n
+
+    
