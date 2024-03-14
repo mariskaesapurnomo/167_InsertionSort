75f5b05e5dc1741838f337331564b8a4c5dac6f5
MariskaEsaPurnomo <mariskaesapurnomo@gmail.com>
2024-03-14 11:20:33 +0700

    mengurutkan Array

--- InsertionSort/InsertionSort.cpp
+++ InsertionSort/InsertionSort.cpp
@@ -7,7 +7,7 @@ int arr[20];                                            // Membuat Array dengan 
 int n;                                                  // Membuat variabel inputan n
 
 void input() {                                          // Procedure Input
-    while (true)                                        
+    while (true)
     {
         cout << "Masukkan Jumlah Data pada Array : ";   // Membuat inputan jumlah element Array
         cin >> n;                                       // Memanggil variabel inputan n
@@ -30,7 +30,31 @@ void input() {                                          // Procedure Input
         cout << "Data Ke-" << (i + 1) << ": ";          // Memasukkan atau menginputkan nilai data ke-n
         cin >> arr[i];                                  // Menyimpan nilai data ke-n kedalam arrray arr
     }
-
 }
 
-    
+    void insertionsort() {				// Procedure Insertionsort
+
+        int temp;						// Membuat variable data temporer atau penyimpanan sementara
+        int i, j;						// Membuat variable j sebagai penanda
+
+        for (i = 1; i <= n - 1; i++) {	// 1. Looping dengan i dimulai dari 1 hingga n-1
+
+            temp = arr[i];				// 2. Simpan nilai arr[i] ke variable sementara temp
+
+            j = i - 1;					// 3. Setting nilai j sama dengan i-1;
+
+            while (j >= 0 && arr[j] > temp)		// 4. Loopping while dimana nilai j lebih besar sama dengan 0 dan
+                // arr[j] lebih besar daripada temp
+            {
+                arr[j + 1] = arr[j];			// 4a. Simpan arr[j] ke dalam variable arr[j+1]
+                j--;							// 4b. Decrement nilai j by 1
+            }
+
+            arr[j + 1] = temp;					// 5. Simpan nilai temp ke dalam arr[j+1]
+
+            cout << "\nPass " << i << ": ";		// Output ke layar
+            for (int k = 0; k < n; k++) {		// Looping nilai k dimulai dari 0 hingga n-1
+                cout << arr[k] << " "; 		// Output ke layar
+            }
+        }
+    }
